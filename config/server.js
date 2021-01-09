7c6643bb12150b2c80a271c61164cc20b6d7c09e
Dino Deck <dino.deck@tamedia.ch>
2021-01-09 14:48:59 +0100

    Revert "deleted components"
    This reverts commit 630bdfd82c3984e858afdef973caab053f272cd3.
    
    undo component delete

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  port: env.int('PORT', 1337),
+  url: env('NGINX_URL', ''),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '255600695ea0c85edb14b49825d3d5f0'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
